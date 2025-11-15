8ab769986d975c2a1212c7058669007c1f1b4cac
saad-alii <98104341+saad-alii@users.noreply.github.com>
2025-11-15 08:02:07 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // teat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+let name = "saad"
+let age = 25
+let isLogedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null); // output object
+console.log(typeof undefined); // output undefined
